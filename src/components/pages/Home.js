8497693bbe88609655b3c365400df5b115b55977
Andrew Ahler <andrewahler@gmail.com>
2021-03-05 05:44:19 -0600

    added about

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+    <p>
+    Recent Vanderbilt University Certified Web Developer with a long history of technical expertise and solving complex IT solutions. Technical savvy includes coding and troubleshooting complex solutions such as cellular devices, whole home, operating systems, Bluetooth, and cloud solutions. 
+    </p>
+  </div>
+);
+
+export default About;
