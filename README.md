3fe860acdddaa9bee838db626370953fb7861c7c
Andrew Ahler <andrewahler@gmail.com>
2021-03-07 14:25:23 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # react-portfolio
+
+### Description
+My web developing portfolio created with React.
+
+![image](https://user-images.githubusercontent.com/71769640/110253613-df930e80-7f50-11eb-8ece-13932f29e6e4.png)
+##### Developed by 
+Andrew Ahler
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,63 @@
 import React from "react";
 
-const Contact = () => (
-<div>
-    <h1>Contact</h1>
-    <p>
-615-519-2636
-</p>
-<p>
-andrewahler@gmail.com
-</p>
-<p>
-https://www.linkedin.com/in/andrew-ahler-287150162/
-</p>
-</div>
-);
+const Contact = () => {
+  return (
+    <div className="contactApp" text color="black">
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            <h3>Contact</h3>
+          </div>
+          <div className="card-body">
+            <blockquote className="blockquote mb-0">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">First and last name</span>
+                </div>
+                <input
+                  type="text"
+                  aria-label="First name"
+                  className="form-control"
+                />
+                <input
+                  type="text"
+                  aria-label="Last name"
+                  className="form-control"
+                />
+              </div>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span
+                    className="input-group-text"
+                    id="inputGroup-sizing-default"
+                  >
+                    Email
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  aria-label="Sizing example input"
+                  aria-describedby="inputGroup-sizing-default"
+                />
+              </div>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span
+                    className="input-group-text"
+                    id="inputGroup-sizing-default"
+                  >
+                    Message
+                  </span>
+                </div>
+                <textarea name="input" id="input" cols="30" rows="5"></textarea>
+              </div>
+              <input className="btn btn-primary" type="submit" value="Submit" />
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Contact;
