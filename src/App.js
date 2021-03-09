87731bd1b8de6038d856046bb243fec06c7649f4
Andrew Ahler <andrewahler@gmail.com>
2021-03-09 14:38:59 -0600

    corrected deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
-
+import background from './images/background.jpg'; 
 const App = () => <PortfolioContainer />;
+
+function App(){
+    return(
+        <div>
+             <img src={background} alt="this is the background image" />
+        </div>
+    )
+}
 
 export default App;
